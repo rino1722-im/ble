d1e97cf41aaaac3161ddefd0420303039acb6395
rino1722-im <81407106+rino1722-im@users.noreply.github.com>
2021-04-08 15:21:56 +0900

    Update ble.js

--- ble.js
+++ ble.js
@@ -81,7 +81,7 @@ async function onButtonClick()
 let btn = document.getElementById('send_button');
 btn.addEventListener('click',sendData );*/
 
-/*function sendData(e){//データ送信の設定
+function sendData(e){//データ送信の設定
   if ( isConnected && characteristics != null ){
     if ( characteristics[0] != null ){
       const encoder = new TextEncoder('utf-8');
@@ -91,6 +91,7 @@ btn.addEventListener('click',sendData );*/
       );
     }
   }
+}
 
 function sendData_r(e){//データ送信の設定
   if ( isConnected && characteristics != null ){
@@ -102,6 +103,7 @@ function sendData_r(e){//データ送信の設定
       );
     }
   }
+}
 
 
   function sendData_b(e){//データ送信の設定
@@ -113,14 +115,15 @@ function sendData_r(e){//データ送信の設定
           char => {ch.startNotifications();}
         );
       }
-    }*/
+    }
+  }
 
 let btn = document.getElementById('send_button');
-//let btn1 = document.getElementById('send_button1');
-//let btn2 = document.getElementById('send_button2');
+let btn1 = document.getElementById('send_button1');
+let btn2 = document.getElementById('send_button2');
 btn.addEventListener('click',sendData );
-//btn1.addEventListener('click',sendData_r );
-//btn2.addEventListener('click',sendData_b );
+btn1.addEventListener('click',sendData_r );
+btn2.addEventListener('click',sendData_b );
 
 
 async function sleep(ms) {
